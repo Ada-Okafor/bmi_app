55a60e2dba6896eb5da50fbcf7fbb08f8b8d5a2b
Ezinwanne Aka <79057350+Chinemelu4@users.noreply.github.com>
2023-05-19 21:08:23 +0100

    Add files via upload

---
+++ bmi_app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from PIL import Image
+
+st.header("My BMI APP")
+
+img = Image.open("bmi1.jpeg")
+st.image(img)
+
+weight = st.number_input("What is your weight(Kg)",step = 0.1)
+height = st.number_input("How tall are you (m)")
+
+def bmi_calc(w,h):
+    bmi = round(w/h**2,2)
+    return bmi
+
+def rating(b):
+    if b > 25:
+        st.write("You are at danger of being overweight")
+    elif 18.4 < b < 25.1:
+        st.write("You are doing well, keep it up")
+    else:
+        st.write("You are at danger of being underweight")
+    
+    
+if st.button("Calculate"):
+    score = bmi_calc(weight,height)
+    st.write(score)
+    rating(score)
+    
